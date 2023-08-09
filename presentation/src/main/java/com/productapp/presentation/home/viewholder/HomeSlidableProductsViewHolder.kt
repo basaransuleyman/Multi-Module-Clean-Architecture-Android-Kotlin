7fe6eaaa3d47bb39d7cf3db0a6bc7879b21947db
Suleyman Basaranoglu <suleyman.basaranoglu@getir.com>
2023-08-10 00:39:37 +0300

    Create Slidable Layout Row, View Holder, Row items and xml files with drawables

---
+++ presentation/src/main/java/com/productapp/presentation/home/viewholder/HomeSlidableProductsViewHolder.kt
@@ -0,0 +1,24 @@
+package com.productapp.presentation.home.viewholder
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.product.common.model.home.ProductItem
+import com.product.sideapp.home.databinding.RowHomeSlidableProductsBinding
+import com.productapp.presentation.home.adapter.HomeSlidableProductAdapter
+
+class HomeSlidableProductsViewHolder(private val binding: RowHomeSlidableProductsBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(productItem: List<ProductItem>, sectionTitle: String) {
+        val productLayoutManager =
+            LinearLayoutManager(binding.root.context, LinearLayoutManager.HORIZONTAL, false)
+
+        binding.homeSlidableSectionTitle.text = sectionTitle
+        binding.homeSlidableRecyclerView.apply {
+            layoutManager = productLayoutManager
+            adapter = HomeSlidableProductAdapter(ArrayList(productItem))
+            (adapter as HomeSlidableProductAdapter).updateList(productItem)
+        }
+
+    }
+}
