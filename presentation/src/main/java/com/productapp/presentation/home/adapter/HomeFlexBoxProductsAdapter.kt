f967ae2e8beff9051b1a8a80be27614c43c043da
Suleyman Basaranoglu <suleyman.basaranoglu@getir.com>
2023-08-10 00:40:12 +0300

    Create Slidable and FlexBox Adapters

---
+++ presentation/src/main/java/com/productapp/presentation/home/adapter/HomeFlexBoxProductsAdapter.kt
@@ -0,0 +1,42 @@
+package com.productapp.presentation.home.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.product.common.model.home.ProductItem
+import com.product.sideapp.home.databinding.HomeFlexBoxProductItemBinding
+import com.productapp.presentation.home.viewholder.HomeFlexBoxProductItemViewHolder
+
+class HomeFlexBoxProductsAdapter(
+    private val list: ArrayList<ProductItem>
+) : RecyclerView.Adapter<HomeFlexBoxProductItemViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): HomeFlexBoxProductItemViewHolder {
+        return HomeFlexBoxProductItemViewHolder(
+            HomeFlexBoxProductItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount() = list.size
+
+    override fun onBindViewHolder(
+        holder: HomeFlexBoxProductItemViewHolder,
+        position: Int
+    ) {
+        val data = list[position]
+        holder.bind(data)
+    }
+
+    fun updateList(newList: List<ProductItem>) {
+        list.clear()
+        list.addAll(newList)
+        notifyDataSetChanged()
+    }
+
+}
---
+++ presentation/src/main/java/com/productapp/presentation/home/adapter/HomeSlidableProductAdapter.kt
@@ -0,0 +1,42 @@
+package com.productapp.presentation.home.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.product.common.model.home.ProductItem
+
+import com.product.sideapp.home.databinding.HomeSlidingProductItemBinding
+import com.productapp.presentation.home.viewholder.HomeSlidableProductItemViewHolder
+
+class HomeSlidableProductAdapter(
+    private val list: ArrayList<ProductItem>
+) : RecyclerView.Adapter<HomeSlidableProductItemViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): HomeSlidableProductItemViewHolder {
+        return HomeSlidableProductItemViewHolder(
+            HomeSlidingProductItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount() = list.size
+
+    override fun onBindViewHolder(
+        holder: HomeSlidableProductItemViewHolder, position: Int
+    ) {
+        val data = list[position]
+        holder.bind(data)
+    }
+
+    fun updateList(newList: List<ProductItem>) {
+        list.clear()
+        list.addAll(newList)
+        notifyDataSetChanged()
+    }
+
+}
