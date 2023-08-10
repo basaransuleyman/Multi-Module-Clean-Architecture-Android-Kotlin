fbccf9eee7609659467fcd8c938ceb98ad5f6bf3
Suleyman Basaranoglu <suleyman.basaranoglu@getir.com>
2023-08-10 13:31:13 +0300

    Add Detail View Model and Fragment Detail xml

---
+++ presentation/src/main/java/com/productapp/presentation/detail/DetailViewModel.kt
@@ -0,0 +1,31 @@
+package com.productapp.presentation.detail
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.product.common.extensions.onFailure
+import com.product.common.extensions.onSuccess
+import com.product.common.model.detail.DetailUIModel
+import com.productapp.domain.usecase.GetDetailUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+    private val getDetailUseCase: GetDetailUseCase
+) : ViewModel() {
+
+    private val _detailFlow = MutableSharedFlow<DetailUIModel?>(extraBufferCapacity = 1)
+    val detailFlow = _detailFlow.asSharedFlow()
+
+    fun getDetail() {
+        viewModelScope.launch {
+            getDetailUseCase(Unit).onSuccess {detailData ->
+                _detailFlow.emit(detailData.data)
+            }.onFailure {
+            }
+        }
+    }
+}
