fe0d44c8f852190546a2c544afe2587f9b76ebcf
Suleyman Basaranoglu <suleyman.basaranoglu@getir.com>
2023-08-11 10:41:51 +0300

    Add UI Model on Common Layer

---
+++ common/src/main/java/com/product/common/model/list/ListUIModel.kt
@@ -0,0 +1,19 @@
+package com.product.common.model.list
+
+import com.product.common.mapper.UIModel
+
+data class ListUIModel(
+    val productList: List<ListProductsUIModel>?,
+    val productLimit: Int?,
+    val  totalCount: Int?
+) : UIModel
+
+data class ListProductsUIModel(
+    val productId: String,
+    val productImage: String,
+    val text: String,
+    val subText: String,
+    val review: String,
+    val questions: String,
+    val rating: String
+) : UIModel
